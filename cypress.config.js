414d64f9659cced584bf65d2b79cc03801c5cf30
Stephanie Penaloza <s.penaloza@a1qa.com>
2025-02-12 21:34:03 -0600

    ci

--- cypress.config.js
+++ cypress.config.js
@@ -2,18 +2,24 @@ const { defineConfig } = require("cypress");
 const browserify = require("@cypress/browserify-preprocessor");
 const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
 const { preprendTransformerToOptions } = require("@badeball/cypress-cucumber-preprocessor/browserify");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer.js');
 
 async function setupNodeEvents(on, config) {
+/*   const on = require('cypress-on-fix')(cypressOn)
+
   await addCucumberPreprocessorPlugin(on, config);
 
-  on("file:preprocessor", browserify(preprendTransformerToOptions(config, browserify.defaultOptions)));
+  on("file:preprocessor", browserify(preprendTransformerToOptions(config, browserify.defaultOptions))); */
+  
+  allureWriter(on, config);
   
   return config;
 }
 
 module.exports = defineConfig({
  e2e: {
-   specPattern: "**/*.feature",
-   setupNodeEvents,
+  // specPattern: "**/*.feature",
+  setupNodeEvents,
+  'env': { 'allure': true }
  },
 });
